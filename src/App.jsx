96c8cd665dfba665c60d6f5c93b300cf98a47103
Victor Cazanave <victor.cazanave@gmail.com>
2018-02-07 20:58:30 +0800

    [App] Rename and set init state

--- src/App.jsx
+++ src/App.jsx
@@ -11,113 +11,21 @@ class App extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			dates: [
-				'2018-01-04',
-				'2018-01-05',
-				'2018-01-06',
-				'2018-01-07',
-				'2018-01-08',
-				'2018-01-09',
-				'2018-01-10'
-			],
-			areas: [
-				{
-					name: 'Taipei City',
-					id: '63'
-				}, {
-					name: 'Kaohsiung City',
-					id: '64'
-				}, {
-					name: 'New Taipei City',
-					id: '65'
-				}, {
-					name: 'Taichung City',
-					id: '66'
-				}, {
-					name: 'Tainan City',
-					id: '67'
-				}, {
-					name: 'Taoyuan City',
-					id: '68'
-				}, {
-					name: 'Matsu Area',
-					id: '09007'
-				}, {
-					name: 'kinmen Area',
-					id: '09020'
-				}, {
-					name: 'Yilan County',
-					id: '10002'
-				}, {
-					name: 'Hsinchu County',
-					id: '10004'
-				}, {
-					name: 'miaoli County',
-					id: '10005'
-				}, {
-					name: 'Changhua County',
-					id: '10007'
-				}, {
-					name: 'Nantou County',
-					id: '10008'
-				}, {
-					name: 'Yunlin County',
-					id: '10009'
-				}, {
-					name: 'Chiayi County',
-					id: '10010'
-				}, {
-					name: 'Pingtung County',
-					id: '10013'
-				}, {
-					name: 'Taitung County',
-					id: '10014'
-				}, {
-					name: 'Hualien County',
-					id: '10015'
-				}, {
-					name: 'Penghu County',
-					id: '10016'
-				}, {
-					name: 'Keelung City',
-					id: '10017'
-				}, {
-					name: 'Hsinchu City',
-					id: '10018'
-				}, {
-					name: 'Chiayi City',
-					id: '10020'
-				}
-			],
-			temperature: {
-				//TODO: Use these values to initialize TemperatureInput
-				min: -10,
-				max: 40
+			init: {
+				dates: [],
+				areas: [],
+				temperature: {
+					min: -100,
+					max: 100
+				},
+				skys: []
 			},
-			skys: [
-				{
-					label: 'Sunny',
-					id: '01'
-				}, {
-					label: 'Partly cloudy',
-					id: '02'
-				}, {
-					label: 'Cloudy',
-					id: '03'
-				}, {
-					label: 'Super cloudy',
-					id: '04'
-				}, {
-					label: 'Raining',
-					id: '05'
-				}
-			],
 			selected: {
 				date: null,
 				areas: [],
 				temperature: {
-					min: -10,
-					max: 40
+					min: -100,
+					max: 100
 				},
 				skys: []
 			},
@@ -126,12 +34,34 @@ class App extends React.Component {
 
 		this.handleSelectDate = this.handleSelectDate.bind(this);
 		this.handleSelectArea = this.handleSelectArea.bind(this);
-		this.handleChangeTemperature = this.handleChangeTemperature.bind(this);
+		this.handleSelectTemperature = this.handleSelectTemperature.bind(this);
 		this.handleSelectSky = this.handleSelectSky.bind(this);
 		this.handleClickGo = this.handleClickGo.bind(this);
 	}
 
-	//TODO: Find a better to handle events
+	componentDidMount() {
+		fetch('init.json').then((response) => {
+			return response.json()
+		}).then((json) => {
+			// Set init state and selected temperature
+			this.setState({
+				init: json,
+				selected: {
+					date: null,
+					areas: [],
+					temperature: {
+						min: json.temperature.min,
+						max: json.temperature.max
+					},
+					skys: []
+				}
+			});
+		}).catch((err) => {
+			console.error('Parsing init.json failed', err)
+		})
+	}
+
+	//TODO: Find a better way to handle events
 
 	handleSelectDate(selectedDate) {
 		this.setState((prevState) => {
@@ -172,14 +102,14 @@ class App extends React.Component {
 		});
 	}
 
-	handleChangeTemperature(changedTemperature) {
+	handleSelectTemperature(selectedTemperature) {
 		this.setState((prevState) => {
 
 			return {
 				selected: {
 					date: prevState.selected.date,
 					areas: prevState.selected.areas,
-					temperature: changedTemperature,
+					temperature: selectedTemperature,
 					skys: prevState.selected.skys
 				}
 			};
@@ -220,8 +150,7 @@ class App extends React.Component {
 				return response.json()
 			}).then((json) => {
 				console.log('parsed json', json)
-				//TODO: Fix location(s) name
-				this.searchMatches(json.area.location, this.state.selected);
+				this.searchMatches(json.area.locations, this.state.selected);
 			}).catch((err) => {
 				console.log('parsing failed', err)
 			})
@@ -237,19 +166,35 @@ class App extends React.Component {
 	}
 
 	match(weather, selected) {
-		return (weather.minTemperature.value >= selected.temperature.min) && (weather.maxTemperature.value <= selected.temperature.max) && (selected.skys.includes(weather.sky));
+		return (weather.minTemperature.value >= selected.temperature.min) && (
+			weather.maxTemperature.value <= selected.temperature.max
+		) && (selected.skys.includes(weather.sky));
 	}
 
 	render() {
-		return (<div>
-			<h1>oeteM</h1>
-			<Summary selected={this.state.selected}/>
-			<When dates={this.state.dates} selectedDate={this.state.selected.date} onSelectDate={this.handleSelectDate}/>
-			<Where areas={this.state.areas} selectedAreas={this.state.selected.areas} onSelectArea={this.handleSelectArea}/>
-			<What temperature={this.state.selected.temperature} onChangeTemperature={this.handleChangeTemperature} skys={this.state.skys} selectedSkys={this.state.selected.skys} onSelectSky={this.handleSelectSky}/>
-			<button onClick={this.handleClickGo}>Let's go!</button>
-			<Matches matches={this.state.matches}/>
-		</div>);
+		return (
+			<div>
+				<h1>oeteM</h1>
+				<Summary selected={this.state.selected}/>
+				<When
+					dates={this.state.init.dates}
+					selectedDate={this.state.selected.date}
+					onSelectDate={this.handleSelectDate}/>
+				<Where
+					areas={this.state.init.areas}
+					selectedAreas={this.state.selected.areas}
+					onSelectArea={this.handleSelectArea}/>
+				<What
+					temperature={this.state.init.temperature}
+					selectedTemperature={this.state.selected.temperature}
+					onSelectTemperature={this.handleSelectTemperature}
+					skys={this.state.init.skys}
+					selectedSkys={this.state.selected.skys}
+					onSelectSky={this.handleSelectSky}/>
+				<button onClick={this.handleClickGo}>Let's go!</button>
+				<Matches matches={this.state.matches}/>
+			</div>
+		);
 	}
 }
 
