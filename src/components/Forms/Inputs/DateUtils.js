5e1431a674a9f3b075ec143bc3b2cfde624958dc
Victor Cazanave <victor.cazanave@gmail.com>
2018-02-10 13:48:08 +0800

    [DateUtils] Create DateUtils to handle dates format

---
+++ src/components/Forms/Inputs/DateUtils.js
@@ -0,0 +1,22 @@
+const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+const _getDate = (string) => {
+	const year = string.slice(0, 4);
+	const month = string.slice(4, 6);
+	const day = string.slice(6, 8);
+
+	return new Date(year, month, day);
+}
+
+//TODO: Keep and display year?
+export function getFormattedDate(string) {
+	const date = _getDate(string);
+
+	return {
+		year: date.getFullYear(),
+		month: MONTHS[date.getMonth()],
+		date: date.getDate(),
+		day: DAYS[date.getDay()]
+	};
+}
