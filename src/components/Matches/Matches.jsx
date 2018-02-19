17d8f4f2f497f7ac17891241afbd92c2ed2bad95
Victor Cazanave <victor.cazanave@gmail.com>
2018-02-19 15:02:57 +0800

    [Matches] Import Component directly

--- src/components/Matches/Matches.jsx
+++ src/components/Matches/Matches.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Summary from './Summary';
 import Match from './Match';
 
-class Matches extends React.Component {
+class Matches extends Component {
 	constructor() {
 		super();
 		this.state = {
