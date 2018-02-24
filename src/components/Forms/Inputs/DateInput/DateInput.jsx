c0dddea005bb80ab648ea5226149b704b41d592b
Victor Cazanave <victor.cazanave@gmail.com>
2018-02-24 23:09:05 +0800

    [DateInput] Transform into dumb component

--- src/components/Forms/Inputs/DateInput/DateInput.jsx
+++ src/components/Forms/Inputs/DateInput/DateInput.jsx
@@ -1,40 +1,27 @@
-import React, {Component} from 'react';
-import {getFormattedDate} from './../../../../Utils/DateUtils.js';
+import React from 'react';
+import { getFormattedDate } from './../../../../Utils/DateUtils.js';
 import './DateInput.css';
 
-class DateInput extends Component {
+function DateInput(props) {
+	const id = `date${props.date}`; // Used to link input and label
+	const formattedDate = getFormattedDate(props.date);
 
-	constructor(props) {
-		super(props);
-		this.formattedDate = getFormattedDate(props.date);
-		this.handleChange = this.handleChange.bind(this);
-	}
-
-	handleChange(event) {
-		this.props.onToggle(event.target.value);
-	}
-
-	render() {
-		// Used to link input and label
-		const id = `date${this.props.date}`;
-
-		return (
-			<div className="date-input">
+	return (
+		<div className="date-input">
 				<input
 					type="radio"
-					value={this.props.date}
-					checked={this.props.selected}
-					onChange={this.handleChange}
+					value={props.date}
+					checked={props.selected}
+					onChange={props.onToggle.bind(this, props.date)}
 					id={id}
 					className="date-input__input"/>
 				<label htmlFor={id} className="date-input__label">
-					<div className="date-input__label__month">{this.formattedDate.month}</div>
-					<div className="date-input__label__date">{this.formattedDate.date}</div>
-					<div className="date-input__label__day">{this.formattedDate.day}</div>
+					<div className="date-input__label__month">{formattedDate.month}</div>
+					<div className="date-input__label__date">{formattedDate.date}</div>
+					<div className="date-input__label__day">{formattedDate.day}</div>
 				</label>
 			</div>
-		);
-	}
+	);
 }
 
 export default DateInput;
