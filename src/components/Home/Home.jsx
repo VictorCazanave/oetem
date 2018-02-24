476de9e271f79ed3a6e379cf532868c47c1441d1
Victor Cazanave <victor.cazanave@gmail.com>
2018-02-24 22:38:49 +0800

    [Home] Fix beautify

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,5 @@
-import React, {
-	Component
-} from 'react';
-import {
-	Link
-} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import About from './About/About';
 import './Home.css';
 
