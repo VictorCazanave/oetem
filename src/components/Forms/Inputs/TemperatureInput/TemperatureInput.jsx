ce63369b15ede47a1ae4855b9b994c576227b756
Victor Cazanave <victor.cazanave@gmail.com>
2018-02-24 23:21:27 +0800

    [TemperatureInput] Transform into dumb component

--- src/components/Forms/Inputs/TemperatureInput/TemperatureInput.jsx
+++ src/components/Forms/Inputs/TemperatureInput/TemperatureInput.jsx
@@ -1,31 +1,19 @@
-import React, {Component} from 'react';
+import React from 'react';
 import InputRange from 'react-input-range';
 import './TemperatureInput.css';
 
-class TemperatureInput extends Component {
+function TemperatureInput(props) {
 
-	constructor() {
-		super();
-		this.handleChange = this.handleChange.bind(this);
-	}
-
-	handleChange(value) {
-		// Need to return temperature object
-		this.props.onChange(value);
-	}
-
-	render() {
-		return (
-			<div className="input-temperature">
+	return (
+		<div className="input-temperature">
 				<InputRange
-					minValue={this.props.min}
-					maxValue={this.props.max}
+					minValue={props.min}
+					maxValue={props.max}
 					formatLabel={value => `${value}°C`}
-					value={this.props.value}
-					onChange={this.handleChange}/>
+					value={props.value}
+					onChange={(value) => props.onChange(value)}/>
 			</div>
-		);
-	}
+	);
 }
 
 export default TemperatureInput;
