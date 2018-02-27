af37b6b162ff2c71f1857fba161226def9e11e03
Victor Cazanave <victor.cazanave@gmail.com>
2018-02-27 10:33:13 +0800

    [Matches] Handle invalid inputs

--- src/components/Matches/Matches.jsx
+++ src/components/Matches/Matches.jsx
@@ -10,48 +10,74 @@ class Matches extends Component {
 	constructor() {
 		super();
 		this.state = {
+			valid: false,
+			fetching: true,
 			matches: []
 		}
 	}
 
 	componentDidMount() {
-		// Convert this.props.selected.skys Set into Array to use find() method
-		const selectedSkys = [...this.props.selected.skys];
+		const valid = this.checkSelected(this.props.selected);
 
-		// TODO: Handle invalid data and display errors (missing date...) for (const selectedArea of
-		// this.props.selected.areas) { const url = `/${this.props.selected.date}_${selectedArea.id}.json`;
-		const url = '20180212_10005.json';
-
-		fetch(url).then((response) => {
-			return response.json()
-		}).then((json) => {
-			this.searchMatches(json.area, this.props.selected.temperature, selectedSkys);
-		}).catch((err) => {
-			console.error(`parsing ${url} failed`, err)
-		})
-		//}
-	}
-
-	searchMatches(area, selectedTemperature, selectedSkys) {
-		const matchedLocations = area.locations.filter(
-			location => this.match(location.weather, selectedTemperature, selectedSkys)
-		).map(matchedLocation => {
-			matchedLocation.area = {
-				id: area.id,
-				name: area.name
-			}
-			return matchedLocation;
+		// Update state.valid
+		this.setState((prevState) => {
+			return update(prevState, {
+				valid: {
+					$set: valid
+				}
+			});
 		});
 
-		if (matchedLocations.length > 0) {
-			this.setState((prevState) => {
-				return update(prevState, {
-					matches: {
-						$push: matchedLocations
-					}
+		// Fetch data and search matches
+		if (valid) {
+			// Convert this.props.selected.skys Set into Array to use find() method
+			const selectedSkys = [...this.props.selected.skys];
+
+			//for (const selectedArea of this.props.selected.areas) {
+			//const url = `/${this.props.selected.date}_${selectedArea.id}.json`;
+			const url = '20180212_10005.json';
+
+			fetch(url).then((response) => {
+				return response.json()
+			}).then((json) => {
+				const matches = this.getMatches(json.area, this.props.selected.temperature, selectedSkys);
+
+				// Must do it here because fetch is async
+				this.setState((prevState) => {
+					return update(prevState, {
+						matches: {
+							$push: matches
+						}
+					});
 				});
-			});
+			}).catch((err) => {
+				console.error(`parsing ${url} failed`, err)
+			})
+			//}
 		}
+	}
+
+	checkSelected(selected) {
+		return true;
+		/*
+		return typeof selected.date === 'string' &&
+			selected.areas.size > 0 &&
+			typeof selected.temperature.min === 'number' &&
+			typeof selected.temperature.max === 'number' &&
+			selected.skys.size > 0;
+		*/
+	}
+
+	getMatches(area, selectedTemperature, selectedSkys) {
+		return area.locations
+			.filter(location => this.match(location.weather, selectedTemperature, selectedSkys))
+			.map(matchedLocation => {
+				matchedLocation.area = {
+					id: area.id,
+					name: area.name
+				}
+				return matchedLocation;
+			});
 	}
 
 	match(weather, selectedTemperature, selectedSkys) {
@@ -71,27 +97,42 @@ class Matches extends Component {
 				<header className="matches-page__header">
 					<h1 className="matches-page__header__title">Matches</h1>
 					<Quote quote="Climate is what we expect, weather is what we get." author="Mark Twain" className="matches-page__header__quote"/>
-					{(sortedMatches.length === 1) && <p className="matches-page__header__subtitle">Here is the perfect place for you:</p>}
 					{
-						(sortedMatches.length > 1) && <p className="matches-page__header__subtitle">
-								{sortedMatches.length}&nbsp;places match your criteria:
-							</p>
+						(!this.state.valid) &&
+						<p className="matches-page__header__subtitle">
+							Hum... It seems that an error occured.
+							<span className="matches-page__header__subtitle__smiley">:-(</span>
+						 	Please try again!
+						</p>
+					}
+					{
+						(this.state.valid && sortedMatches.length === 0) &&
+						<p className="matches-page__header__subtitle">
+							Sorry, no place matches your criteria.
+							<span className="matches-page__header__subtitle__smiley">:-/</span>
+							Please try again later or with less strict criteria!
+						</p>
+					}
+					{
+						(this.state.valid && sortedMatches.length === 1) &&
+						<p className="matches-page__header__subtitle">
+							Here is the perfect place for you:
+						</p>
+					}
+					{
+						(this.state.valid && sortedMatches.length > 1) &&
+						<p className="matches-page__header__subtitle">
+							{sortedMatches.length}&nbsp;places match your criteria:
+						</p>
 					}
 				</header>
 
-				{
-					(sortedMatches.length === 0) && <div className="matches-page__empty">
-							<p>Sorry, no place matches your criteria</p>
-							<p>You may try again with more open criteria</p>
-						</div>
-				}
-				{
-					(sortedMatches.length > 0) && <ul className="matches-page__list">{
-								sortedMatches.map(
-									(match) => (<li className="matches-page__list__item" key={match.name}><Match match={match}/></li>)
-								)
-							}</ul>
-				}
+				<ul className="matches-page__list">
+					{
+						(this.state.valid) &&
+						sortedMatches.map((match) => (<li className="matches-page__list__item" key={match.name}><Match match={match}/></li>))
+					}
+				</ul>
 
 				<footer className="matches-page__footer">
 					<Link to="/" className="matches-page__footer__button-home">Home</Link>
