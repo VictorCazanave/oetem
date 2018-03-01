ea46fa106e4c5bcc8f4034daa7ebd94bde2717c4
Victor Cazanave <victor.cazanave@gmail.com>
2018-03-01 10:38:06 +0800

    [Spinner] Transform into dumb component

--- src/components/Common/Spinner/Spinner.jsx
+++ src/components/Common/Spinner/Spinner.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import './Spinner.css';
 
-class Spinner extends React.Component {
-
-	render() {
-		return (
-			<div className="spinner"></div>
-		);
-	}
+function Spinner(props) {
+	return (
+		<div className="spinner"></div>
+	);
 }
 
 export default Spinner;
