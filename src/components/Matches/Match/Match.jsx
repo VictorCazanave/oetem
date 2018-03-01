082811c90e28b17a7c580d87dd58e00664132353
Victor Cazanave <victor.cazanave@gmail.com>
2018-03-01 12:54:21 +0800

    [Match] Create LinkIcon, IconForecast and IconMap components

--- src/components/Matches/Match/Match.jsx
+++ src/components/Matches/Match/Match.jsx
@@ -1,49 +1,39 @@
 import React from 'react';
+import IconMap from './IconMap/IconMap';
+import IconForecast from './IconForecast/IconForecast';
 import './Match.css';
-import forecastImg from './forecast.png';
-import mapImg from './map.png';
 
-class Match extends React.Component {
+function Match(props) {
 
-	render() {
-		const forecastUrl =
-			`http://www.cwb.gov.tw/V7e/forecast/town368/town2.php?townid=${this.props.match.geocode}`;
-		const mapUrl = `https://www.google.com/maps?q=${this.props.match.lat},${this.props.match.lon}`
-
-		return (
-			<div className="match">
+	return (
+		<div className="match">
 				<div className="match__location">
 					<div className="match__location__name">
-						{this.props.match.name}
+						{props.match.name}
 					</div>
 					<ul className="match__location__criteria">
 						<li className="match__location__criteria__item">
-							{this.props.match.area.name}
+							{props.match.area.name}
 						</li>
 						<li className="match__location__criteria__item">
-							{this.props.match.weather.minTemperature.value}°{this.props.match.weather.minTemperature.unit}
-							&nbsp;/&nbsp;{this.props.match.weather.maxTemperature.value}°{this.props.match.weather.maxTemperature.unit}
+							{props.match.weather.minTemperature.value}°{props.match.weather.minTemperature.unit}
+							&nbsp;/&nbsp;{props.match.weather.maxTemperature.value}°{props.match.weather.maxTemperature.unit}
 						</li>
 						<li className="match__location__criteria__item">
 							<img
-								src={`http://www.cwb.gov.tw/V7/symbol/weather/gif/day/${this.props.match.weather.sky.id}.gif`}
-								alt={this.props.match.weather.sky.label}
-								title={this.props.match.weather.sky.label}
+								src={`http://www.cwb.gov.tw/V7/symbol/weather/gif/day/${props.match.weather.sky.id}.gif`}
+								alt={props.match.weather.sky.label}
+								title={props.match.weather.sky.label}
 								className="match__location__criteria__item__image"/>
 						</li>
 					</ul>
 				</div>
 				<div className="match__more">
-					<a href={mapUrl} target="_blank" rel="noopener noreferrer" className="match__more__link">
-						<img src={mapImg} alt="Map icon" className="match__more__link__image"/>
-					</a>
-					<a href={forecastUrl} target="_blank" rel="noopener noreferrer" className="match__more__link">
-						<img src={forecastImg} alt="Forecast icon" className="match__more__link__image"/>
-					</a>
+					<IconMap lat={props.match.lat} lon={props.match.lon} />
+					<IconForecast geocode={props.match.geocode} />
 				</div>
 			</div>
-		);
-	}
+	);
 }
 
 export default Match;
