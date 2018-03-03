185ceaffd5428675dbff9e11f529ba9b7f20b27d
Victor Cazanave <victor.cazanave@gmail.com>
2018-03-03 21:44:12 +0800

    [utils] Fix case problem

---
+++ src/utils/DateUtils.js
@@ -0,0 +1,31 @@
+const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+/**
+ * Transform string date into Date object 
+ * @param  {String} string Date with YYYYMMDD format
+ * @return {[type]}        Date object
+ */
+const _getDate = (string) => {
+	const year = parseInt(string.slice(0, 4), 10);
+	const month = parseInt(string.slice(4, 6), 10) - 1; // month is 0-based
+	const day = string.slice(6, 8);
+
+	return new Date(year, month, day);
+};
+
+/**
+ * Return a custom object with right date format
+ * @param  {String} string Date with YYYYMMDD format 
+ * @return {Object}        Date formatted values
+ */
+export function formatDate(string) {
+	const date = _getDate(string);
+
+	return {
+		year: date.getFullYear(),
+		month: MONTHS[date.getMonth()],
+		date: date.getDate(),
+		day: DAYS[date.getDay()]
+	};
+}
