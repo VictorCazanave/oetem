8afb7d63f28c2b128660267566ef4258171552ae
Victor Cazanave <victor.cazanave@gmail.com>
2018-03-16 21:49:41 +0800

    [MapInput] Finish styles

--- src/components/Forms/Inputs/MapInput/MapInput.jsx
+++ src/components/Forms/Inputs/MapInput/MapInput.jsx
@@ -9,20 +9,19 @@ function MapInput(props) {
 	return (
 		<svg className="map-input" xmlns="http://www.w3.org/2000/svg" viewBox="312 322 688 973" aria-label="Map of Taiwan">
 		{
-			areas.map((area) => {
-				const selected = props.isSelected ? props.isSelected(area) : undefined;
-
-				return (<path
+			areas.map(area =>
+				(<path
 					className="map-input__area"
 					id={area.id}
 					d={area.path}
-					onClick={props.onClick}
+					onClick={props.onAreaClick}
 					role={role}
 					aria-label={area.name}
-					aria-selected={selected}
+					aria-selected={props.isAreaSelected(area)}
+					tabIndex="0"
 					key={area.id} />
-				);
-			})
+				)
+			)
 		}
 	</svg>
 	);
